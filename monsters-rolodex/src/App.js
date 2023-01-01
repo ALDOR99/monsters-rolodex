e75235620f9eca7aa9c7e1eb2506aac54a4e61a6
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-01-01 20:39:08 +0300

    .1,0
    :alien:

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,25 @@
-import Component from "react";
-
+import { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 //--------------------------------------------------------------------
 
-class App extends Component() {
+class App extends Component {
+  constructor() {
+    //inşaatçi
+    super();
+    this.state = {
+      name: "Yihua",
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>hello everyone</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>hello {this.state.name}</p>
+          <button>Change Name</button>
         </header>
       </div>
     );
