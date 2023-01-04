7cf3144ca5e65c50dc448517aa3126220a0ee438
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-01-05 02:15:11 +0300

    .1,1
    :alien:

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -5,22 +5,51 @@ import "./App.css";
 //--------------------------------------------------------------------
 
 class App extends Component {
+  //------------------------------------------------------------------------------------------------------------------
+
   constructor() {
     //inşaatçi
     super();
     this.state = {
-      name: "Yihua", //isim bilgisi
+      monsters: [],
     };
   }
+
+  //------------------------------------------------------------------------------------------------------------------
+
+  componentDidMount() {
+    //[yaşam döngüsü yöntemi  = lifecycle method]
+
+    fetch("https://jsonplaceholder.typicode.com/users") //Jsonu alıyorum ,[ fetch = gidip getirmek ]
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  //------------------------------------------------------------------------------------------------------------------
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>hello {this.state.name}</p>
-          <button>Change Name</button>
-        </header>
+        {
+          //dizi yönetimi "map" = dizinin içinde,soldan sağa doğru her bir öğe üzerinde,
+          //yineleme yapmamızı sağlayan bir yöntemdir.Ve size yeni bir dizi ,geriye verir.
+          this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            ); //monster parametresini geriye return et, h etiketi içerisinde
+          })
+        }
       </div>
     );
   }
