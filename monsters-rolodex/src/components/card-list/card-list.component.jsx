344d5e8787e66a7521b5646a8800533d85e33c54
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-01-12 21:23:29 +0300

    .1,3
    :alien:

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import "./card-list.styles.css";
 
 //-----------------------------------------------------------
 
@@ -14,11 +15,25 @@ class CardList extends Component {
     const { monsters } = this.props; //Bu yapıyı oluşturmamın nedeni,bunu birden fazla yerde kullanmamız gerekirse, bunu yapmanın kolay yolu
     //----------------------------------------------------
 
+    //alt = alt metni erişilebilirlik içindir ve esasen ekran okuyucularında bunun ne olduğu belirtecektir
     return (
       <div className="card-list">
-        {monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        {monsters.map((monster) => {
+          const { name, email, id } = monster; // Küçük bir optimizasyon
+
+          return (
+            <div key={id} className="card-container">
+              <img
+                alt={`monster ${name}`}
+                src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+              />
+
+              <h2>{name}</h2>
+
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </div>
     ); // kapsül içinde birden fazla ana div olamaz
   }
